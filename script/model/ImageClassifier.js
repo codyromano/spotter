5f3d03432f9a4f8cc398afefd1ffb409152c98e7
Cody Romano <codyromano@gmail.com>
2018-09-02 23:24:31 -0700

    Implement scheduler to improve mobile perf

--- script/model/ImageClassifier.js
+++ script/model/ImageClassifier.js
@@ -14,9 +14,18 @@ export default class ImageClassifier {
     this.classMetadata = {};
     this.mapClassIdToIndex = {};
     this.examples = 0;
+
+    this.addImageStack = [];
+    this.imageStackProcessing = false;
+    this.imageStackProcessInterval = 1000;
+    this.imageStackBatchLimit = 10;
+
+    this.processImageStack = this.processImageStack.bind(this);
+    this.imageStackCount = {};
   }
 
   addClass(id, label) {
+    this.imageStackCount[id] = 0;
     this.classMetadata[this.classMetadataIndex] = { id, label };
     this.mapClassIdToIndex[id] = this.classMetadataIndex;
     this.classMetadataIndex += 1;
@@ -29,16 +38,44 @@ export default class ImageClassifier {
 
   addImage(imageNode, classMetadataId) {
     this.examples += 1;
+    this.imageStackCount[classMetadataId] = ++this.imageStackCount[classMetadataId];
 
     const index = this.mapClassIdToIndex[classMetadataId];
     const imagePixelData = dl.fromPixels(imageNode);
-    this.knn.addImage(imagePixelData, index);
-    imagePixelData.dispose();
+    // this.knn.addImage(imagePixelData, index);
+    this.addImageStack.push({
+      imagePixelData,
+      index
+    })
+
+    if (!this.imageStackProcessing) {
+      this.imageStackProcessing = true;
+      this.processImageStack();
+    }
+  }
+
+  processImageStack() {
+    let limit = 0;
+    while (this.addImageStack.length && limit < this.imageStackBatchLimit) {
+      const { imagePixelData, index } = this.addImageStack.pop();
+      this.knn.addImage(imagePixelData, index);
+      imagePixelData.dispose();
+      limit += 1;
+    }
+
+    if (this.addImageStack.length) {
+      window.setTimeout(this.processImageStack, this.imageStackProcessInterval);
+    } else {
+      this.imageStackProcessing = false;
+    }
   }
 
   getExamplesForClass(classId) {
+    return this.imageStackCount[classId];
+    /*
     const classMetadataIndex = this.mapClassIdToIndex[classId];
     return this.knn.getClassExampleCount()[classMetadataIndex];
+    */
   }
 
   // TODO: async / await
